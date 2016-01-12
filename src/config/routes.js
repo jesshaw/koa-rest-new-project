ce9a72545d7df9220920c29e0d3f58edbdb9f6ab
unknown <568658857@qq.com>
2016-01-12 22:55:57 +0800

    test cross domain post

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,20 +6,22 @@
 
 var mount = require('koa-mount');
 var koajwt = require('koa-jwt');
+var bodyParser = require('koa-bodyparser');
+var jsonp = require('koa-safe-jsonp');
 
 // var profile = {
-        //     id: 123
-        // };
+//     id: 123
+// };
 
-        // var token = koajwt.sign(profile, 'secret', {
-        //     expiresInMinutes: 60 * 5 //设置超时时间为5小时
-        // });
+// var token = koajwt.sign(profile, 'secret', {
+//     expiresInMinutes: 60 * 5 //设置超时时间为5小时
+// });
 
-        // var generatorToken = function(profile) {
-        //     koajwt.sign(profile, 'secret', {
-        //         expiresInMinutes: 60 * 5 //设置超时时间为5小时
-        //     });
-        // }
+// var generatorToken = function(profile) {
+//     koajwt.sign(profile, 'secret', {
+//         expiresInMinutes: 60 * 5 //设置超时时间为5小时
+//     });
+// }
 
 
 
@@ -28,11 +30,20 @@ var koajwt = require('koa-jwt');
 // console.log('  curl -H "Authorization: Bearer ' + token + '" http://localhost:3000/cars   # should succeed (return " 200 protected")'); //
 
 module.exports = function(app) {
+    jsonp(app, {
+        callback: 'callback', // default is 'callback'
+        limit: 50, // max callback name string length, default is 512
+    });
+
+    app.use(bodyParser());
+
     // Custom 401 handling
     app.use(function*(next) {
         try {
+            console.log(this.request.body);
             yield next;
         } catch (err) {
+            console.log(err);
             if (401 === err.status) {
                 this.status = 401;
                 this.body = '401 Unauthorized - Protected resource, use Authorization header to get access\n';
--- src/resources/account/account.controller.js
+++ src/resources/account/account.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// var koajwt = require('koa-jwt');
+var koajwt = require('koa-jwt');
 
 // var profile = {
 //     id: 123
@@ -23,14 +23,15 @@ exports.index = function*(next) {
     this.status = 200;
     this.body = [];
     // return yield next;
-
 };
 
 
-//$ curl -d '{"userName":"aaa","password":"111111"}' http://localhost:8080/account/login
+// curl -H "Content-Type: application/json" -X POST -d '{"username":"xyz","password":"xyz"}' http://localhost:8080/account/login
 exports.login = function*(next) {
-    // console.log(this.method);
     if ('POST' !== this.method) return yield next;
+    console.log(this.request.body);
     this.status = 200;
-    this.body = "token";
+    this.body = koajwt.sign(this.request.body, 'secret', {
+        expiresInMinutes: 60 * 5 //设置超时时间为5小时
+    });
 };
