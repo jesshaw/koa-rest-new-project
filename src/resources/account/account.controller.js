262478a06d52095bbbe158b1b1eb9e39d49e3247
mzxiao <mzxiao@Ctrip.com>
2016-01-13 16:39:41 +0800

    interceptor add authorization header

--- src/resources/account/account.controller.js
+++ src/resources/account/account.controller.js
@@ -15,7 +15,19 @@ exports.login = function*(next) {
     if ('POST' !== this.method) return yield next;
     // console.log(this.request.body);
     this.status = 200;
-    this.body = koajwt.sign(this.request.body, 'secret', {
-        expiresInMinutes: 60 * 5 //设置超时时间为5小时
-    });
+    var res = {};
+    if (this.request.body && this.request.body.userLogin && this.request.body.userLogin.userName === 'a@a.com' && this.request.body.userLogin.password === '111111') {
+        res = {
+            status: true,
+            token: koajwt.sign(this.request.body, 'secret', {
+                expiresInMinutes: 60 * 5 //设置超时时间为5小时
+            })
+        };
+        this.body = res;
+    } else {
+        res = {
+            status: false
+        };
+        this.body = res;
+    }
 };
--- src/resources/cars/cars.controller.js
+++ src/resources/cars/cars.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-
+// curl http://localhost:8080/cars
 exports.index = function*(next) {
     this.status = 200;
-    this.body = [];
+    this.body = ['aa', 'bb', 'cc'];
 };
